190754a60d79ee962afc8e14290f31ce37e00891
Dulio Denis <dulio.denis@yahoo.com>
2016-10-12 12:00:41 -0400

    Added a UICollectionView with programmatic auto-layout

---
+++ Login/Login/Controllers/LoginController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginController.swift
+//  Login
+//
+//  Created by Dulio Denis on 10/12/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .green
+        return cv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        collectionView.anchorToTop(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+}
+
+
+extension UIView {
+    
+    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top: top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    
+    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -rightConstant).isActive = true
+        }
+    }
+    
+}
