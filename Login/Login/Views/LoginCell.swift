e52f03a3ba2789d4685554502519cfb99d10378d
Dulio Denis <dulio.denis@yahoo.com>
2016-10-22 13:30:23 -0400

    Programmatically laid out Login Page UI

--- Login/Login/Views/LoginCell.swift
+++ Login/Login/Views/LoginCell.swift
@@ -10,13 +10,78 @@ import UIKit
 
 class LoginCell: UICollectionViewCell {
     
+    let logoImageView: UIImageView = {
+        let image = UIImage(named: "logo")
+        let imageView = UIImageView(image: image)
+        return imageView
+    }()
+    
+    let emailTextField: LeftPaddedTextField = {
+        let textField = LeftPaddedTextField()
+        textField.layer.cornerRadius = 4.0
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1
+        textField.placeholder = "email"
+        textField.keyboardType = .emailAddress
+        return textField
+    }()
+    
+    let passwordTextField: LeftPaddedTextField = {
+        let textField = LeftPaddedTextField()
+        textField.layer.cornerRadius = 4.0
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1
+        textField.placeholder = "password"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 4.0
+        button.backgroundColor = UIColor.rgb(41, 128, 185)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Login", for: .normal)
+        return button
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
+        
+        addSubview(logoImageView)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginButton)
+        
+        _ = logoImageView.anchor(top: centerYAnchor, left: nil, bottom: nil, right: nil, topConstant: -200, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 160, heightConstant: 160)
+        logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        _ = emailTextField.anchor(top: logoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 10, leftConstant: 30, bottomConstant: 0, rightConstant: 30, widthConstant: 0, heightConstant: 50)
+        
+        _ = passwordTextField.anchor(top: emailTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 15, leftConstant: 30, bottomConstant: 0, rightConstant: 30, widthConstant: 0, heightConstant: 50)
+        
+        _ = loginButton.anchor(top: passwordTextField.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 15, leftConstant: 30, bottomConstant: 0, rightConstant: 30, widthConstant: 0, heightConstant: 50)
     }
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+// Subclass UITextField to add some left padding
+
+class LeftPaddedTextField: UITextField {
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: bounds.origin.x + 10, y: bounds.origin.y, width: bounds.width + 10, height: bounds.height)
     }
     
 }
